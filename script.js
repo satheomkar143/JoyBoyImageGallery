6cd25c94bf5d24e5e9e3ddc41663b1210b7ca9ae
omkarsathe <satheomkar143@gmail.com>
2022-11-10 09:39:37 +0530

    random images by user search

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // unsplash api
 // documentation get random photo
 
+let = searchParam = location.search.split("=").pop();
 const access_key = "rNVwC0_oge_yeUsY4qrMIPsRztIj8yTVI-sezUu4LxM";
 const random_photo_URL = `https://api.unsplash.com/photos/random?client_id=${access_key}&count=50`;
+const search_photo_URL = `https://api.unsplash.com/search/photos?client_id=${access_key}&query=${searchParam}&per_page=50`;
 
 const gallery = document.querySelector(".gallery");
+
 let allImages;
-let currentImage=0;
+let currentImage = 0;
 
 const getImages = () => {
   fetch(random_photo_URL)
@@ -14,6 +17,16 @@ const getImages = () => {
     .then((data) => {
       console.log(data);
       allImages = data;
+      makeImages(allImages);
+    });
+};
+
+const searchImages = () => {
+  fetch(search_photo_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      allImages = data.results;
       makeImages(allImages);
     });
 };
@@ -30,43 +43,60 @@ const makeImages = (data) => {
     gallery.appendChild(div);
 
     // popup image
-    img.addEventListener("click",()=>{
+    img.addEventListener("click", () => {
       currentImage = index;
       showPopup(item);
-    })
+    });
   });
 };
 
-const showPopup =(item)=>{
+const showPopup = (item) => {
   let popup = document.querySelector(".img-popup-window");
   const downloadBtn = document.querySelector(".download-btn");
   const closeBtn = document.querySelector(".close-btn");
-  const image= document.querySelector(".large-img");
+  const image = document.querySelector(".large-img");
 
   popup.classList.remove("hidden");
   downloadBtn.href = item.links.html;
   image.src = item.urls.regular;
 
-  closeBtn.addEventListener("click",()=>{
+  closeBtn.addEventListener("click", () => {
     popup.classList.add("hidden");
-  })
-}
+  });
+};
 
 const preBtn = document.querySelector(".pre-btn");
 const nextBtn = document.querySelector(".next-btn");
 
-preBtn.addEventListener("click",()=>{
-  if(currentImage>0){
+preBtn.addEventListener("click", () => {
+  if (currentImage > 0) {
     currentImage--;
-    showPopup(allImages[currentImage]) ;
+    showPopup(allImages[currentImage]);
   }
-})
+});
 
-nextBtn.addEventListener("click",()=>{
-  if(currentImage<allImages.length-1){
+nextBtn.addEventListener("click", () => {
+  if (currentImage < allImages.length - 1) {
     currentImage++;
-    showPopup(allImages[currentImage]) ;
+    showPopup(allImages[currentImage]);
   }
-})
+});
 
-getImages()
+if (searchParam == "") {
+  getImages();
+} else {
+  searchImages();
+}
+
+// --------------create clock-----------
+const clock = document.getElementById('clock');
+const day= ['sun','mon','tue','wed','thu','fri','sat'];
+function start() {
+  interval = setInterval(function () {
+    var d = new Date();
+    var time = d.toLocaleTimeString();
+    let getday = d.getDay();
+    clock.innerHTML = day[getday]+' '+ time;
+  }, 1000);
+}
+start();
