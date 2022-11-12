102b2d7a3b3745e214733abad86baff34a35cf0a
omkarsathe <satheomkar143@gmail.com>
2022-11-12 22:01:21 +0530

    image carousel created

--- profile.js
+++ profile.js
@@ -9,8 +9,81 @@ editBtn.addEventListener("click", () => {
   editDiv.classList.remove("hide-info");
 });
 
-saveBtn.addEventListener("click", ()=>{
+saveBtn.addEventListener("click", () => {
   displayDiv.classList.remove("hide-info");
   editDiv.classList.add("hide-info");
   saveBtn.classList.add("hide-info");
-})
+});
+
+// -------------------------------------------------
+// ----------------carasol----------------
+const access_key = "rNVwC0_oge_yeUsY4qrMIPsRztIj8yTVI-sezUu4LxM";
+const random_photo_URL = `https://api.unsplash.com/photos/random?client_id=${access_key}&count=10`;
+
+let currentImage = 0;
+const carasol = document.querySelector(".img-carasol");
+let allImages;
+
+const getImages = () => {
+  fetch(random_photo_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      allImages = data;
+      // makeImages(allImages);
+    });
+};
+
+const makeImages = (data) => {
+
+    // console.log(data);
+
+    let img = document.createElement("img");
+    img.src = data.urls.regular;
+    img.className = "large-img";
+    carasol.innerHTML = "";
+    carasol.appendChild(img);
+
+};
+
+function update() {
+  if (currentImage < allImages.length - 1) {
+    currentImage++;
+    console.log("in update" + currentImage);
+    makeImages(allImages[currentImage]);
+  } else {
+    currentImage = 0;
+  }
+}
+
+getImages();
+var interval;
+
+function start() {
+  interval = setInterval(update, 3000);
+  console.log("in function update");
+}
+// start();
+
+function stop() {
+  console.log("stopping");
+  clearInterval(interval);
+}
+
+const clickBtn = document.querySelector(".clickme");
+const stopBtn = document.querySelector(".stopme");
+
+clickBtn.addEventListener("click", () => {
+  // alert("click me!");
+  start();
+  clickBtn.classList.add("hide-info");
+  stopBtn.classList.remove("hide-info");
+});
+
+stopBtn.addEventListener("click", () => {
+  // alert("click me!");
+  stop();
+  carasol.innerHTML = "";
+  clickBtn.classList.remove("hide-info");
+  stopBtn.classList.add("hide-info");
+});
